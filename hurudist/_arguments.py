1d7d3786ed55513975a1e2f83b08a6fe64e2aeaa
Adam Johnson <AdamJohnso@gmail.com>
2019-09-29 00:32:13 -0400

    Fix help casing to be consistent.

--- hurudist/_arguments.py
+++ hurudist/_arguments.py
@@ -21,21 +21,21 @@ program_description = "H'uru Asset Distribution Manager"
 main_parser = argparse.ArgumentParser(description=program_description)
 
 log_group = main_parser.add_mutually_exclusive_group()
-log_group.add_argument("-q", "--quiet", action="store_true", help="Only print critical information")
-log_group.add_argument("-v", "--verbose", action="store_true", help="Print verbose log output")
+log_group.add_argument("-q", "--quiet", action="store_true", help="only print critical information")
+log_group.add_argument("-v", "--verbose", action="store_true", help="print verbose log output")
 
 sub_parsers = main_parser.add_subparsers(title="Command", dest="command", required=True)
 
 # Package age command argment parser
 age_parser = sub_parsers.add_parser("age")
-age_parser.add_argument("age_name", type=str, help="Name of the age to bundle")
-age_parser.add_argument("source", type=Path, help="Path to the root of the Plasma client")
-age_parser.add_argument("destination", type=Path, help="Path to store the resulting asset bundle")
+age_parser.add_argument("age_name", type=str, help="name of the age to bundle")
+age_parser.add_argument("source", type=Path, help="path to the root of the Plasma client")
+age_parser.add_argument("destination", type=Path, help="path to store the resulting asset bundle")
 
 age_parser.add_argument("--dataset", type=lambda x: Dataset[x], default=Dataset.base, choices=list(Dataset),
-                        help="Dataset this age belongs to")
+                        help="dataset this age belongs to")
 age_parser.add_argument("--distribute", type=lambda x: Distribute[x], choices=list(Distribute),
-                        help="Ability to redistribute this asset package")
-age_parser.add_argument("--moul-scripts", type=Path, help="Path to the moul-scripts repository for this client")
-age_parser.add_argument("--python", type=Path, help="Path to the python interpreter executable used by this client")
-age_parser.add_argument("--skip-pfm-dependencies", action="store_true", help="Don't include Python dependency modules and SDLs in this package")
+                        help="ability to redistribute this asset package")
+age_parser.add_argument("--moul-scripts", type=Path, help="path to the moul-scripts repository for this client")
+age_parser.add_argument("--python", type=Path, help="path to the python interpreter executable used by this client")
+age_parser.add_argument("--skip-pfm-dependencies", action="store_true", help="don't include Python dependency modules and SDLs in this package")
